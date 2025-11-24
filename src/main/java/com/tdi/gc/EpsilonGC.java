330ba734b081d4c4f5b1c78d790d788cb2f65752
Терешко Даниил Игоревич <daniiltereshko123@gmail.com>
2025-11-24 19:02:41 +0300

    EpsilonGC

---
+++ src/main/java/com/tdi/gc/EpsilonGC.java
@@ -0,0 +1,16 @@
+package com.tdi.gc;
+
+public class EpsilonGC {
+
+    //-XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -Xms64m -Xmx64m -Xlog:gc*::time -Xlog:gc=debug:file=gc.text
+    public static void main(String[] args) {
+        System.out.println("Start the program");
+        System.out.println("Start the loop");
+
+        for (int i = 200; i < 200_000_000; i++) {
+            String string = String.valueOf(i);
+        }
+
+        System.out.println("End the loop");
+    }
+}
